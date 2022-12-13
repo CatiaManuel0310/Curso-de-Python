6d3de18468cd09df29bd8f6900270ea1804b6dd7
manuelcatia20@gmail.com <manuelcatia20@gmail.com>
2022-12-13 10:13:25 -0300

    quinto commit

--- CursoemVideo/Ex35.py
+++ CursoemVideo/Ex35.py
@@ -6,6 +6,6 @@ r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 =float(input('Terciro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 +r2:
-   print('Os segmentos acima podem formar um triangulo')
+   print('Os segmentos acima podem formar um triângulo')
 else:
-    print('Os segmentos acima não podem formar um triangulo')
+    print('Os segmentos acima não podem formar um triângulo')
